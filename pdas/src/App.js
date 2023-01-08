38c610ae69b922eb3f6c6dd7995c018f6c3b9f1f
Karan Meghe <karanmeghe87@gmail.com>
2023-01-08 06:33:14 +0530

    Styling done with help of bulma and mini project is complete

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,12 +1,50 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImg from "./images/alexa.png";
+import CortanaImg from "./images/cortana.png";
+import SiriImg from "./images/siri.png";
 
 const App = () => {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa44" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri99" />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa44"
+                image={AlexaImg}
+                description="Alexa was created by Amazon helps you buy things"
+              />
+            </div>
+
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImg}
+                description="Cortana made by Microsoft. Who knows what it does? "
+              />
+            </div>
+
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri99"
+                image={SiriImg}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,9 +1,23 @@
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image, description }) => {
   //   const { title, handle } = props;
   return (
-    <div>
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="Personal digital assistance logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+
+        <div className="content">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 };
